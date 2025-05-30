ea5fd3d8010d76b4359b2403363b9b2d096dc066
Hamidulla9 <mahamadjonxoldorov9@gmail.com>
2025-05-30 19:12:07 +0500

    11

--- dopi/models.py
+++ dopi/models.py
@@ -1,29 +1,47 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 import random
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError("Email kiritilishi kerak")
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
-        user.set_unusable_password()
-        user.save()
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+        user.save(using=self._db)
         return user
 
-class User(AbstractBaseUser):
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser uchun is_staff=True bo\'lishi kerak.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser uchun is_superuser=True bo\'lishi kerak.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100, blank=True)
     image = models.ImageField(upload_to='users/', blank=True, null=True)
     phone = models.CharField(max_length=20, unique=True, blank=True, null=True)
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)        # Admin panelga kirish uchun
     is_verified = models.BooleanField(default=False)
 
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name']  # superuser yaratishda kerak bo‘ladigan qo‘shimcha maydonlar
 
     def __str__(self):
         return self.email
@@ -38,4 +56,3 @@ class VerificationCode(models.Model):
         if not self.code:
             self.code = str(random.randint(10000, 99999))
         super().save(*args, **kwargs)
-
